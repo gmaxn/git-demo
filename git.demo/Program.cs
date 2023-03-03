862c294d8fe8c1f14abeef1a7905aa29771a430b
gmaxn <mguelpa@gmail.com>
2023-03-03 11:26:22 -0300

    initial commit

---
+++ git.demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git.demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
